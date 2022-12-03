694a949b71bd5bac3e637539b72a2e948126879d
Sunny7079 <sunnysahu930496@gmail.com>
2022-12-03 13:30:05 +0530

    masai

--- index.js
+++ index.js
@@ -1,34 +1,94 @@
+
+
 let getdata=async(e)=>{
+   let  url="https://server-products.onrender.com/products"
+
     let form=document.getElementById("form")
     e.preventDefault()
 
-    let name=form.name
-    let brand=form.brand
-    let price=form.price
-    let image=form.image
+    let name=form.name.value
+    let brand=form.brand.value
+    let price=+form.price.value
+    let image=form.image.value
 
     let product={
- name:name.value,
- brand:brand.value,
- price:+price.value,
- image:image.value,
+ name,
+ brand,
+ price,
+ image,
+ isbuyed:"false"
 
 
     }
     console.log(product)
-let url="https://serverd.onrender.com/products"
     await fetch(url,{
         method:"POST",
         body:JSON.stringify(product),
         headers:{
-            "Content-Type":"appplication/json"
+            "Content-Type":"application/json"
         },
     })
     form.reset()
+    mydata()
 }
 let mydata=async()=>{
-    let res=await fetch(`https://serverd.onrender.com/products`)
+    let res=await fetch(`https://server-products.onrender.com/products`)
     res=await res.json()
     console.log(res)
+    append(res)
 }
+
 mydata()
+
+let append=(data)=>{
+
+let cont=document.getElementById("container")
+cont.innerHTML=null
+    data.forEach((e)=>{
+        let div=document.createElement("div")
+        let name=document.createElement("h2")
+        name.textContent=e.name
+        let brand=document.createElement("h2")
+        brand.textContent=e.brand
+        let price=document.createElement("p")
+        price.textContent=e.price
+        let image=document.createElement("img")
+        image.src=e.image
+        let p=document.createElement("p")
+        p.textContent=e.isbuyed
+    let remove=document.createElement("button")
+    remove.textContent="remove"
+    let toggle=document.createElement("button")
+    toggle.textContent="toggle"
+
+toggle.addEventListener("click",async()=>{
+ var isbuyed=!isbuyed
+ 
+  await fetch(`https://server-products.onrender.com/products`,{
+      method:"PATCH",
+      body:JSON.stringify(isbuyed),
+      headers:{
+          "Content-Type":"application/json"
+      },
+  })
+  mydata()
+  
+
+})
+
+    remove.addEventListener("click",async()=>{
+        let res=await fetch(`https://server-products.onrender.com/products/${e.id}`,{method:"DELETE"})
+        console.log(e.id)
+        mydata()
+
+    })
+
+        div.append(image,name,brand,price,p,remove,toggle)
+        cont.append(div)
+    
+    })
+
+  
+ 
+}
+
